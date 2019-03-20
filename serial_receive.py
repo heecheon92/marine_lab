725314390af9cf062526fd5ea82a5ce7005c2a06
Heecheon <heecheon92@gmail.com>
2019-03-20 00:07:27 -0500

    slight modification

--- serial_receive.py
+++ serial_receive.py
@@ -1,19 +1,15 @@
-"""
-author: Heecheon Park
-Marine Lab Project
-Language: Python 2.7
-Description:
-    This is a base program to receive signals from Arduino.
-    Signal is converted to a string and concatenate with a datetime object
-    to record a timestamp for each data received.
-"""
-
 import serial
 import datetime
+import subprocess
 
-ser = serial.Serial('/dev/ttyUSB0',9600)
+cmd = "dmesg |egrep 'attached*' |cut -d ' ' -f10"
+serialName = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+serialName = str(serialName.communicate()[0]).replace('\n', '').replace('\r', '')
+
+ser = serial.Serial('/dev/'+serialName,9600)
 s = [0]
 while True:
+
         str1 = str(ser.readline())
         str1 = str1.replace('\n', ' ').replace('\r', '')
         print str1+str(datetime.datetime.now())[:16]
